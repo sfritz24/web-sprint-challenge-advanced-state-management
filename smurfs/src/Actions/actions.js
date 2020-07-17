cbfe6957d15ba897150389b68fd11fd5d4b1ce58
shanon <13fritsh@gmail.com>
2020-07-17 11:37:13 -0500

    worked in the app.js, added post request inputchange and submit handler

--- smurfs/src/Actions/actions.js
+++ smurfs/src/Actions/actions.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 export const FETCH_SMURFS_START = 'FETCH_SMURFS_START';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
 export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
+export const POST_SMURFS = 'POST_SMURFS';
 
 export const fetchSmurfs = () =>{
     return dispatch =>{
         dispatch({type: FETCH_SMURFS_START});
-        axios.get('https://pokeapi.co/api/v2/SMURFS?limit=10')
+        axios.get('http://localhost:3333/smurfs')
             .then(response =>{
                 dispatch({type: FETCH_SMURFS_SUCCESS, payload: response.data.results})
             })
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 import "./App.css";
+
+const initialFormValues = {
+  name: '',
+  age: '',
+  height: '',
+}
 
 export const initialState ={
   isLoading: false,
@@ -7,17 +14,49 @@ export const initialState ={
   error: ''
 };
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+const App = () => {
+
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [smurf, setSmurf] = useState(initialState.smurfs)
+
+  const postSmurf = (newSmurf) =>{
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+      .then(response =>{
+        setSmurf([...smurf, response.data])
+      })
+      .catch(error =>{
+        console.log(error)
+      })
+      .finally(() =>{
+        setFormValues(initialFormValues)
+      })
+  };
+
+  const onInputChange = (event) =>{
+    const {name, value} = event.target;
+    setFormValues({...formValues, [name]: value})
+  };
+
+   const onSubmit = (event) =>{
+    event.preventDefault();
+
+    const newSmurf = {
+      name: formValues.name,
+      age: formValues.age,
+      height: formValues.height,
+    };
+
+    postSmurf(newSmurf)
+  };
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+    </div>
+  );
 }
 
 export default App;
